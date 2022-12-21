901c36e380e79308b47b90e03528b4507293452a
mjeammet <marie.jeammet@protonmail.com>
2022-12-21 15:52:28 +0100

    debug and allowed hosts settings

--- config/settings.py
+++ config/settings.py
@@ -28,10 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.getenv("DEBUG") == "True" else False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["127.0.0.1", os.getenv("HOST_URL")]
 
 # Application definition
 
