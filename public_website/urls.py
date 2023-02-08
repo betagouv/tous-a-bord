d2f431b80eb77fe03f461d4dbb73823fabd46a5e
BenoitSerrano <benoit.serrano10@gmail.com>
2023-02-08 17:04:46 +0100

    add connection to moncomptepro

--- public_website/urls.py
+++ public_website/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
     path("", views.index_view, name="index"),
     path("accessibilite/", views.accessibility_view, name="accessibilite"),
-    path("artois-mobilites/", views.pe_status_view, name="artoismobi")
+    path("artois-mobilites/", views.pe_status_view, name="artoismobi"),
+    path("login/", views.login_view, name="login"),
+    path('oidc/', include('mozilla_django_oidc.urls')),
 ]
