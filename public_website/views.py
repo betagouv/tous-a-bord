c83a98a9aa5e237bc5e08e4107ea7465bede23f1
Marie J <marie.jeammet@beta.gouv.fr>
2023-02-08 15:32:36 +0100

    improve layout for API response

--- public_website/views.py
+++ public_website/views.py
@@ -16,6 +16,7 @@ def accessibility_view(request):
 
 def pe_status_view(request):
     inscription_data = None
+    form = InscritPoleEmploi
     APIPART_ENDPOINT = (
         "https://particulier-test.api.gouv.fr/api/v2/situations-pole-emploi"
     )
@@ -32,7 +33,7 @@ def pe_status_view(request):
             inscription_data = response.json()
 
     context = {
-        "form": InscritPoleEmploi,
+        "form": form,
         "inscription_data": inscription_data,
     }
     return render(request, "public_website/pe_status.html", context)
