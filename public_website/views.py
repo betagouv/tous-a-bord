0c1dab39e654c5af8e8c6447f670a1e999debc65
Marie J <marie.jeammet@beta.gouv.fr>
2023-03-29 17:17:40 +0200

    redirects from views instead of decorators

--- public_website/views.py
+++ public_website/views.py
@@ -1,11 +1,10 @@
+from django.contrib import messages
+from django.contrib.auth.models import Group
 from django.shortcuts import redirect, render
 
-from public_website.decorators import (
-    authorization_required_message,
-    login_required_message,
-)
+from public_website.decorators import login_required_message
 from public_website.forms import InscritPoleEmploiForm, StatutEtudiantBoursierForm
-from public_website.models import APICall
+from public_website.models import APICall, Habilitation
 
 
 def index_view(request):
@@ -28,10 +27,19 @@ def login_view(request):
 
 
 @login_required_message()
-@authorization_required_message(group_name="Artois Mobilités")
+# @authorization_required_message(group_name="Artois Mobilités")
 def pole_emploi_status_view(request):
     inscription_data = None
     form = InscritPoleEmploiForm
+    authorized_group = Group.objects.get(name="Artois Mobilités")
+
+    if (
+        authorized_group not in request.user.groups.all()
+        or not Habilitation.objects.filter(group=authorized_group).exists()
+    ):
+        error_message = "Vous êtes bien connecté·e, mais vous n'avez pas les droits pour accéder à cette page."
+        messages.error(request, error_message)
+        return redirect("/services/")
 
     if request.method == "POST":
         form = InscritPoleEmploiForm(request.POST)
@@ -58,10 +66,19 @@ def pole_emploi_status_view(request):
 
 
 @login_required_message()
-@authorization_required_message(group_name="Brest Métropole")
+# @authorization_required_message(group_name="Brest Métropole")
 def etudiant_boursier_status_view(request):
     inscription_data = None
     form = StatutEtudiantBoursierForm
+    authorized_group = Group.objects.get(name="Brest Métropole")
+
+    if (
+        authorized_group not in request.user.groups.all()
+        or not Habilitation.objects.filter(group=authorized_group).exists()
+    ):
+        error_message = "Vous êtes bien connecté·e, mais vous n'avez pas les droits pour accéder à cette page."
+        messages.error(request, error_message)
+        return redirect("/services/")
 
     if request.method == "POST":
         form = StatutEtudiantBoursierForm(request.POST)
