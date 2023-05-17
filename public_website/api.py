b2413095f0de4a4700b07eeeb6f5330086968d41
Marie J <marie.jeammet@beta.gouv.fr>
2023-05-17 16:38:42 +0200

    send mail through webhook

--- public_website/api.py
+++ public_website/api.py
@@ -5,10 +5,15 @@ from django.utils import timezone
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 
+from public_website.utils.email_provider import send_webhook_notification_email
+
 
 @csrf_exempt
 @require_POST
 def webhook(request):
     payload = json.loads(request.body)
     print(f"- {timezone.now()}: {payload}")
+    send_webhook_notification_email(
+        payload["to"], message=payload["type_de_l_email_a_envoyer"]
+    )
     return HttpResponse("OK", content_type="text/plain")
--- public_website/utils/email_provider.py
+++ public_website/utils/email_provider.py
@@ -3,7 +3,7 @@ import os
 import sib_api_v3_sdk
 
 configuration = sib_api_v3_sdk.Configuration()
-configuration.api_key["api-key"] = os.environ["SEND_IN_BLUE_API_KEY"]
+configuration.api_key["api-key"] = os.environ["BREVO_API_KEY"]
 
 
 def send_user_creation_email(user_email: str):
@@ -16,6 +16,37 @@ def send_user_creation_email(user_email: str):
         + user_email
         + " vient de se créer un compte sur la plateforme. Vous pouvez retrouver la liste des comptes créés à l'adresse https://tous-a-bord.beta.gouv.fr/admin"
     )
+    try:
+        api_instance = sib_api_v3_sdk.TransactionalEmailsApi(
+            sib_api_v3_sdk.ApiClient(configuration)
+        )
+        to = [sib_api_v3_sdk.SendSmtpEmailTo(to_email)]
+        sender = sib_api_v3_sdk.SendSmtpEmailSender(
+            name=sender_name, email=sender_email
+        )
+        email = sib_api_v3_sdk.SendSmtpEmail(
+            sender=sender, to=to, text_content=text_content, subject=subject
+        )
+        api_instance.send_transac_email(email)
+        return True
+    except sib_api_v3_sdk.rest.ApiException as e:
+        print("Exception when calling ContactsApi->create_contact: %s", e)
+        raise
+
+
+def send_webhook_notification_email(email_adress: str, message: str):
+    send_transac_email(
+        sender_email="tousabord@beta.gouv.fr",
+        sender_name="Tous à bord",
+        to_email=email_adress,
+        subject="Test webhook Tous à Bord !",
+        text_content=message,
+    )
+
+
+def send_transac_email(
+    sender_email: str, sender_name: str, to_email: str, subject: str, text_content: str
+) -> bool:
     try:
         api_instance = sib_api_v3_sdk.TransactionalEmailsApi(
             sib_api_v3_sdk.ApiClient(configuration)
